af592d6e929ab860ca063e970ba8eca40e99450f
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-02-17 03:44:29 +0200

    catch base vars, create base functions

---
+++ second-video-player/script.js
@@ -0,0 +1,20 @@
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
+
+// Play & pause video
+function toggleVideoStatus() {
+  return true;
+}
+
+// Update play/pause icon
+function updatePlayIcon() {
+  return true;
+}
+
+// Update progress & timestamp
+function updateProgress() {
+  return true;
+}
